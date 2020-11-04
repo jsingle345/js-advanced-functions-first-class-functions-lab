dcd0cb222fdf9cd02942daabea919be5d145be07
jsingle345 <jsingle345@gmail.com>
2020-11-04 11:57:43 -0600

    commit tree 328bca0eeae1076781dab193c74fbb93880b4d89

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers=(drivers)=>{
+    return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers=(drivers)=>{
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier=(outsideNum)=>{
+        return function(insideNum){
+           return outsideNum * insideNum
+    }
+}
+
+const fareDoubler=(fare)=>{
+    return createFareMultiplier(2)(fare)
+}
+
+const fareTripler=(fare)=>{
+    return createFareMultiplier(3)(fare)
+}
+
+const selectDifferentDrivers=(array, method)=>{
+    return method(array)
+}
